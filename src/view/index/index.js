5e0b70ad4b0df6667175918baf7e377ae685f7c3
Journey <1274049755@qq.com>
2019-05-26 21:26:08 +0800

    响应式适配

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,34 +1,25 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom"
 import {Menu, Row, Col} from "antd"
+import IndexMenu from "./indexMenu"
 
 
 class Index extends Component{
     render(){
         return <Row className="wrap">
-            <Col md={6}>
-                <Menu id="indexMenu">
-                    <Menu.Item>
-                        <Link to="/index/ask">全部</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/index/good">精华</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/index/ask">问题</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/index/share">分享</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/index/jobk">招聘</Link>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Link to="/index/test">测试</Link>
-                    </Menu.Item>
-                </Menu>
+            <Col md={6} xs={0} className="indexSider">
+                <IndexMenu
+                    id="indexMenu"
+                    mode="vertical"
+                />
             </Col>
-            <Col md={18} style={{
+            <Col md={0} xs={24}>
+                <IndexMenu
+                    id="indexXsMenu"
+                    mode="horizontal"
+                />   
+            </Col>
+            <Col md={18} xs={24} style={{
                 height:"500px"
             }} className="indexList"></Col>
         </Row>
---
+++ src/view/index/indexMenu.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import {Link} from "react-router-dom"
+import {Menu} from "antd"
+
+class IndexMenu extends Component {
+    render() {
+        return <Menu 
+                    id={this.props.id}
+                    mode={this.props.mode}
+                >
+                <Menu.Item>
+                    <Link to="/index/ask">全部</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to="/index/good">精华</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to="/index/ask">问题</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to="/index/share">分享</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to="/index/jobk">招聘</Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link to="/index/test">测试</Link>
+                </Menu.Item>
+            </Menu>
+    }
+}
+
+export default IndexMenu
